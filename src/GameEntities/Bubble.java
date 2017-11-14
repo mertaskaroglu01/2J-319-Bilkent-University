7a5d135f0181eb1eddb74cf259de2c55804ef6e8
FaaizHaque <32203451+FaaizHaque@users.noreply.github.com>
2017-11-15 00:56:57 +0300

    Testing and updated Bubble.java

--- src/GameEntities/Bubble.java
+++ src/GameEntities/Bubble.java
@@ -3,9 +3,18 @@ package GameEntities;
 public class Bubble extends GameObject {
 	
 	int speed;
+	int points;
 	
 	Bubble( int bubbleType) {
-		
+		//0 = red
+		//1 = yellow
+		//2 = green
+		if ( bubbleType == 0 )
+			points = 10;
+		else if ( bubbleType == 1)
+			points = 20;
+		else 
+			points = 50;
 	}
 	
 	public void changeSpeed( int roundId) {
