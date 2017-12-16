a959612b2001eed0d9951142012bec0188f59830
FaaizHaque <32203451+FaaizHaque@users.noreply.github.com>
2017-12-16 21:40:04 +0300

    Help and Credits Panel design changes

--- src/UserInterface/CreditsPanel.java
+++ src/UserInterface/CreditsPanel.java
@@ -1,5 +1,14 @@
 package UserInterface;
 
+import java.awt.Graphics;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -7,17 +16,51 @@ import javax.swing.JPanel;
 public class CreditsPanel extends JPanel {
 	
 	
-	JLabel txt1;
 	JButton btnCreditsBack;
+	JLabel text1;
+	JLabel text2;
+	
 	/**
 	 * Create the panel.
 	 */
 	public CreditsPanel() {
 		setVisible(true);
-		txt1 = new JLabel("bla bla");
-		add(txt1);
-		btnCreditsBack = new JButton("Back to Main Menu");
+		
+		ImageIcon credits1 = new ImageIcon("C:\\Users\\Faaiz\\git\\2J-BubblePopper\\pictures\\credits1.png");
+		ImageIcon credits2 = new ImageIcon("C:\\Users\\Faaiz\\git\\2J-BubblePopper\\pictures\\credits2.png");
+		ImageIcon back = new ImageIcon("C:\\Users\\Faaiz\\git\\2J-BubblePopper\\pictures\\back.png");
+		
+		this.setLayout(new GridLayout(3,1));
+		
+		text1 = new JLabel();
+		text2 = new JLabel();
+		text1.setHorizontalAlignment(JLabel.CENTER);
+		text2.setHorizontalAlignment(JLabel.CENTER);
+		
+		text1.setIcon(credits1);
+		text2.setIcon(credits2);
+	
+		btnCreditsBack = new JButton(back);
+		btnCreditsBack.setOpaque(false);
+		btnCreditsBack.setContentAreaFilled(false);
+		btnCreditsBack.setBorderPainted(false);
+		
+		add(text1);
+		add(text2);
 		add(btnCreditsBack);
+	}
+	
+	public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        //Add background picture to main menu
+        try {
+        	final BufferedImage background = ImageIO.read(new File("C:\\Users\\Faaiz\\git\\2J-BubblePopper\\pictures\\menubackground.jpg"));
+        	g.drawImage(background, 0, 0, this);	
+        }
+        catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 }
--- src/UserInterface/HelpPanel.java
+++ src/UserInterface/HelpPanel.java
@@ -1,18 +1,59 @@
 package UserInterface;
 
+import java.awt.GridLayout;
+
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class HelpPanel extends JPanel {
 	
 	JButton btnHelpBack;
+	JLabel player1;
+	JLabel player2;
+	JLabel ctrl1;
+	JLabel ctrl2;
+	
 	/**
 	 * Create the panel.
 	 */
 	public HelpPanel() {
 		setVisible(true);
-		btnHelpBack = new JButton("Back to Main Menu");
-		add(btnHelpBack);
+		
+		ImageIcon back = new ImageIcon("C:\\Users\\Faaiz\\git\\2J-BubblePopper\\pictures\\back.png");
+		ImageIcon controls1 = new ImageIcon("C:\\Users\\Faaiz\\git\\2J-BubblePopper\\pictures\\controls1.png");
+		ImageIcon controls2 = new ImageIcon("C:\\Users\\Faaiz\\git\\2J-BubblePopper\\pictures\\controls2.png");
+		ImageIcon arrows = new ImageIcon("C:\\Users\\Faaiz\\git\\2J-BubblePopper\\pictures\\arrows.png");
+		ImageIcon wasd = new ImageIcon("C:\\Users\\Faaiz\\git\\2J-BubblePopper\\pictures\\wasd.png");
+		
+		this.setLayout(new GridLayout(3,2) );
+		
+		player1 = new JLabel();
+		player2 = new JLabel();
+		ctrl1 = new JLabel();
+		ctrl2 = new JLabel();
+		
+		player1.setIcon(controls1);
+		player2.setIcon(controls2);
+		ctrl1.setIcon(arrows);
+		ctrl2.setIcon(wasd);
+		
+		player1.setHorizontalAlignment(JLabel.CENTER);
+		player2.setHorizontalAlignment(JLabel.CENTER);
+		ctrl1.setHorizontalAlignment(JLabel.CENTER);
+		ctrl2.setHorizontalAlignment(JLabel.CENTER);
+				
+		btnHelpBack = new JButton(back);
+		btnHelpBack.setOpaque(false);
+		btnHelpBack.setContentAreaFilled(false);
+		btnHelpBack.setBorderPainted(false);
+		
+		this.add(player1);
+		this.add(player2);
+		this.add(ctrl1);
+		this.add(ctrl2);
+		this.add(btnHelpBack);
 		
 	}
 
