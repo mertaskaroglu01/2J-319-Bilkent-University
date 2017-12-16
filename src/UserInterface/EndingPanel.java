7436e8311248b42fd2a741046dfd1c3e7a7b971d
FaaizHaque <32203451+FaaizHaque@users.noreply.github.com>
2017-12-16 23:19:22 +0300

    Design changes to ending and inter panels.

--- src/UserInterface/EndingPanel.java
+++ src/UserInterface/EndingPanel.java
@@ -1,7 +1,15 @@
 package UserInterface;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -13,7 +21,9 @@ public class EndingPanel extends JPanel {
 	JButton btnSaveScore;
 	JButton btnEndingBack;
 	int[] messages;
-	JLabel info;
+	JLabel info1;
+	JLabel info2;
+	JLabel gameOver;
 	/**
 	 * Create the panel.
 	 */
@@ -26,26 +36,57 @@ public class EndingPanel extends JPanel {
 		}
 		
 		this.setVisible(true);
-		btnSaveScore = new JButton("Save Scores");
-		this.add(btnSaveScore);
-		btnEndingBack = new JButton("Back to Main Menu");
+		this.setLayout(new GridLayout(4,0));
+		
+		ImageIcon game = new ImageIcon("C:\\Users\\Faaiz\\git\\2J-BubblePopper\\pictures\\gameover.png");
+		ImageIcon back = new ImageIcon("C:\\Users\\Faaiz\\git\\2J-BubblePopper\\pictures\\back.png");
+		ImageIcon save = new ImageIcon("C:\\Users\\Faaiz\\git\\2J-BubblePopper\\pictures\\savescore.png");
+		
+		
+		btnSaveScore = new JButton(save);
+		btnEndingBack = new JButton(back);
+
+		btnSaveScore.setOpaque(false);
+		btnSaveScore.setContentAreaFilled(false);
+		btnSaveScore.setBorderPainted(false);
+		btnEndingBack.setOpaque(false);
+		btnEndingBack.setContentAreaFilled(false);
+		btnEndingBack.setBorderPainted(false);
+
+		info1 = new JLabel( "Player 1 Score: " + messages[0] );
+		info2 = new JLabel( "Player 2 Score: " + messages[1] );
+		gameOver = new JLabel();
+		gameOver.setIcon(game);
+		
+		info1.setFont(new Font("Courier New", Font.BOLD, 35));
+		info2.setFont(new Font("Courier New", Font.BOLD, 35));
+		
+		info1.setHorizontalAlignment(JLabel.CENTER);
+		info2.setHorizontalAlignment(JLabel.CENTER);
+		gameOver.setHorizontalAlignment(JLabel.CENTER);
+		info1.setForeground(Color.WHITE);
+		info2.setForeground(Color.WHITE);
+		
+		this.add(gameOver);
+		this.add(info1);
+		this.add(info2);
 		this.add(btnEndingBack);
-		String temp = "";
-		for( int i = 0; i < 3 ; i++) {
-			temp = temp + messages[i] + " ";
-		}
-	info = new JLabel( temp);
-		this.add(info);
 		
 	}
 	
 	public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        	String temp = "";
-			for( int i = 0; i < 3 ; i++) {
-				temp = temp + messages[i] + " ";
+        	super.paintComponent(g);
+			info1.setText("Player 1 Score: " + messages[0]);
+			info2.setText("Player 2 Score: " + messages[1]);
+			
+			try {
+	        	final BufferedImage background = ImageIO.read(new File("C:\\Users\\Faaiz\\git\\2J-BubblePopper\\pictures\\menubackground.jpg"));
+	        	g.drawImage(background, 0, 0, this);	
+	        }
+	        catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-			info.setText(temp);
     }
 
 }
--- src/UserInterface/InterPanel.java
+++ src/UserInterface/InterPanel.java
@@ -1,13 +1,22 @@
 package UserInterface;
 
 import java.awt.Graphics;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.Timer;
+
+import java.awt.Color;
+import java.awt.Font;
 
 public class InterPanel extends JPanel {
 	
@@ -15,7 +24,9 @@ public class InterPanel extends JPanel {
 	JButton btnProceed;
 	JButton btnInterBack;
 	int[] messages;
-	JLabel info;
+	JLabel info1;
+	JLabel info2;
+	JLabel info3;
 	/**
 	 * Create the panel.
 	 */
@@ -28,27 +39,59 @@ public class InterPanel extends JPanel {
 		}
 		
 		this.setVisible(true);
-		btnProceed = new JButton("Proceed Next Round");
-		this.add(btnProceed);
-		btnInterBack = new JButton("Back to Main Menu");
-		this.add(btnInterBack);
-		String temp = "";
-		for( int i = 0; i < 3 ; i++) {
-			temp = temp + messages[i] + " ";
-		}
-	info = new JLabel( temp);
-		this.add(info);
+		this.setLayout(new GridLayout(5,0));
 		
+		ImageIcon nextlevel = new ImageIcon("C:\\Users\\Faaiz\\git\\2J-BubblePopper\\pictures\\nextlevel.png");
+		ImageIcon back = new ImageIcon("C:\\Users\\Faaiz\\git\\2J-BubblePopper\\pictures\\back.png");
+		btnProceed = new JButton(nextlevel);
+		btnInterBack = new JButton(back);
+		
+		
+		btnProceed.setOpaque(false);
+		btnProceed.setContentAreaFilled(false);
+		btnProceed.setBorderPainted(false);
+		btnInterBack.setOpaque(false);
+		btnInterBack.setContentAreaFilled(false);
+		btnInterBack.setBorderPainted(false);
+
+		info1 = new JLabel( "Player 1 Score: " + messages[0] );
+		info2 = new JLabel( "Player 2 Score: " + messages[1] );
+		info3 = new JLabel( "Lives Remaining:" + messages[2] );
+		info1.setHorizontalAlignment(JLabel.CENTER);
+		info2.setHorizontalAlignment(JLabel.CENTER);
+		info3.setHorizontalAlignment(JLabel.CENTER);
+		info1.setForeground(Color.WHITE);
+		info2.setForeground(Color.WHITE);
+		info3.setForeground(Color.WHITE);
+		
+		info1.setFont(new Font("Courier New", Font.BOLD, 35));
+		info2.setFont(new Font("Courier New", Font.BOLD, 35));
+		info3.setFont(new Font("Courier New", Font.BOLD, 35));
+		
+		this.add(btnProceed);
+		this.add(info1);
+		this.add(info2);
+		this.add(info3);
+		this.add(btnInterBack);
 	}
 	
 	public void paintComponent(Graphics g) {
-        super.paintComponent(g);
+        	super.paintComponent(g);
         	String temp = "";
 			for( int i = 0; i < 3 ; i++) {
 				temp = temp + messages[i] + " ";
 			}
-			info.setText(temp);
-			
+			info1.setText("Player 1 Score: " + messages[0]);
+			info2.setText("Player 2 Score: " + messages[1]);
+			info3.setText("Lives Remaining: " + messages[2]);
+			 try {
+		        	final BufferedImage background = ImageIO.read(new File("C:\\Users\\Faaiz\\git\\2J-BubblePopper\\pictures\\menubackground.jpg"));
+		        	g.drawImage(background, 0, 0, this);	
+		        }
+		        catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
     }
 	
 }
